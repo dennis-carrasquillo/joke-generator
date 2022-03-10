23c37998239669f0d2ee42e72d4cc7fc8d1715c2
Dennis Carrasquillo <spycydennis@gmail.com>
2022-03-10 10:28:47 -0500

    Created basic structure of joke page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>J2G Joke Generator</h1>
+      <div className="joke">
+
+      </div>
+      <div className="generate">
+        <button>Generate Joke</button>
+      </div>
     </div>
   );
 }
