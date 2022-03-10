55b9e0ab1fea51d9076b607b6b47c4ebf5c5835d
Dennis Carrasquillo <spycydennis@gmail.com>
2022-03-10 12:39:50 -0500

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Joke from './components/Joke';
+import axios from 'axios';
 
 function App() {
+
+  const[joke, setJoke] = useState("")
+
+  const addJoke = (e) => {
+    e.preventDefault();
+    fetch('https://v2.jokeapi.dev/joke/Programming')
+    .then(res => {
+      if(res.ok === true)  {
+        return res.json();
+      }
+      throw res;
+    })
+    .then(data => {
+      if(data.type === 'single'){
+        setJoke(data.joke);
+      } else{
+        setJoke(data.setup)
+        setTimeout(()=>{setJoke(data.delivery)}, 3000);
+      }
+    })
+    .catch(err => {console.error(err);});
+  }
+  
   return (
     <div className="App">
       <h1>J2G Joke Generator</h1>
       <div className="joke">
-
+        <Joke joke={joke}/> 
       </div>
       <div className="generate">
-        <button>Generate Joke</button>
+        <button onClick={addJoke}>Generate Joke</button>
       </div>
     </div>
   );
---
+++ src/components/Joke.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Joke({joke}) {
+    return (
+        <div>
+            <h2>{joke}</h2>
+        </div>
+    );
+}
+
+export default Joke;
